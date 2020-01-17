7685b5dc83ac6bb4c38396416efef68ed6725465
Catherine Barnett <ca.barnett06@gmail.com>
2020-01-17 15:42:55 -0500

    adding start up prompt

--- server.js
+++ server.js
@@ -15,3 +15,47 @@ connection.connect(function(err) {
     start();
 });
 
+// function which prompts the user for what action they should take
+function start() {
+    inquirer
+      .prompt({
+        name: "task",
+        type: "list",
+        message: "Would you like to do?",
+        choices: ["view employees", "view managers", "view departments", "view roles", "add employee", "add department", "add role",
+                    "update employee roll", "EXIT"]
+      })
+      .then(function(answer) {
+        
+        switch (answer.task){
+            case "view employees":
+                viewEmps();
+                break;
+            case "view managers":
+                viewMan();
+                break;
+            case "view departments":
+                viewDeps();
+                break;
+            case "view roles":
+                viewRoles();
+                break;
+            case "add employee":
+                addEmp();
+                break;
+            case "add department":
+                addDep();
+                break;
+            case "add role":
+                addRole();
+                break;
+            case "update employee roll":
+                updateEmp();
+                break;
+            case "EXIT":
+                connection.end();
+                break;
+        }
+      });
+  }
+
